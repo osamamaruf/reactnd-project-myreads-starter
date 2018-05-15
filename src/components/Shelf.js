48483d27d21fd62ade0b1bf0d5d17746a5f304c1
syedosamamaruf <syedosamamaruf>
2018-05-16 01:01:30 +0500

    feat:  create props for shelf component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -3,12 +3,20 @@ import Book from './Book'
 
 class Shelf extends React.Component {
     render() {
+        const title = this.props.title;
+        const books = this.props.books;
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">title</h2>
+                <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        <Book></Book>
+                        {books.map((book) => (
+                            <li key={book.title}>
+                                <Book title={book.title} author={book.author} cover={book.cover}></Book>
+                            </li>
+                        ))
+
+                        }
                     </ol>
                 </div>
             </div>
