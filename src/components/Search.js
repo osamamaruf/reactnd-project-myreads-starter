294c851286470d946ada409ba50a6a83d559e51a
syedosamamaruf <syedosamamaruf>
2018-05-19 09:07:37 +0500

    feat: Populate search grid based on values provided in search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,30 +1,60 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import SearchGrid from './SearchGrid'
+import * as BooksAPI from '../BooksAPI'
 
 class Search extends React.Component {
     state = {
-        query: ''
+        query: '',
+        books: []
     }
 
     updateQuery = (query) => {
         this.setState(
             () => ({
-                query: query.trim()
-            })
-        )
+                query: query
+            }),
+            () => { this.fetchSearchedBooks(this.state.query) }
+        );
 
     }
 
-    clearQuery = () => {
-        this.updateQuery('')
+    fetchSearchedBooks(query) {
+        query === '' ?
+            this.setState(() => ({
+                books: []
+            })) :
+            BooksAPI.search(query)
+                .then((books) => (
+                    books.error ? [] : books.map((book) => {
+                        if (!book.shelf) {
+                            book.shelf = 'none';
+                        }
+
+                        return book;
+                    })
+                ))
+                .then((books) => {
+                    this.setState(() => ({
+                        books
+                    }))
+
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.setState(() => ({
+                        books: []
+                    }))
+                });
     }
 
     render() {
+        const { query, books } = this.state;
+
         return (
-            <div className="search-books">
-                <SearchBar query={this.state.query} updateQuery={this.updateQuery}></SearchBar>
-                <SearchGrid></SearchGrid>
+            <div className="search-books" >
+                <SearchBar query={query} updateQuery={this.updateQuery}></SearchBar>
+                <SearchGrid books={books}></SearchGrid>
             </div>
         );
     };
--- src/components/SearchGrid.js
+++ src/components/SearchGrid.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Book from './Book'
 
 class SearchGrid extends React.Component {
     render() {
+        const books = this.props.books;
         return (
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    {books
+                        .map((book) => (
+                            <li key={book.id}>
+                                <Book
+                                    title={book.title}
+                                    authors={book.authors ? book.authors : []}
+                                    smallThumbnail={book.imageLinks ? book.imageLinks.smallThumbnail : undefined}
+                                    shelf={book.shelf}
+                                    id={book.id}                               >
+                                </Book>
+                            </li>
+                        ))
+
+                    }
+                </ol>
             </div>
         );
     };
